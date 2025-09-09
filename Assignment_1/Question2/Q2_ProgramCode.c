aab461e4b9d91a174124fb3d7b9f29f97bbb521a
S M Mahboor Hussain <K250749@nu.edu.pk>
2025-09-10 02:32:00 +0500

    Add files via upload

---
+++ Assignment_1/Question2/Q2_ProgramCode.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    float x, y;
+    printf("Coordinates are represented in the manner (x,y)\n");
+    printf("Enter the x-Coordinate: ");
+    scanf("%f",&x);
+    printf("Enter the y-Coordinate: ");
+    scanf("%f",&y);
+    if ((x>0)&&(y>0)){
+        printf("Point lies in Quadrant I");
+    } else if ((x>0)&&(y<0)){
+        printf("Point lies in Quadrant IV");
+    } else if ((x<0)&&(y>0)){
+        printf("Point lies in Quadrant II");
+    } else if ((x<0)&&(y<0)){
+        printf("Point lies in Quadrant III");
+    } else{
+        printf("Point lies on the Axes!");
+    }
+}
